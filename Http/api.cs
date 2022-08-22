65ca61127e0fed0d77ca300e84afc51f84e6e528
walumo <miikka.hakulinen@awacademy.training>
2022-08-22 16:47:14 +0300

    GetGeniusAsync code updated

--- Http/api.cs
+++ Http/api.cs
@@ -9,14 +9,20 @@ namespace musicsearch.Http
     {
         public static async Task<string> GetGeniusAsync()
         {
-            var client = new HttpClient();
+            var clientHandler = new HttpClientHandler
+            {
+                UseCookies = false,
+                AutomaticDecompression = DecompressionMethods.GZip,
+            };
+            var client = new HttpClient(clientHandler);
             var request = new HttpRequestMessage
             {
                 Method = HttpMethod.Get,
-                RequestUri = new Uri("http://api.genius.com/search?q=finished%20with%20my%20woman"),
+                RequestUri = new Uri("https://api.genius.com/search?q=finished"),
                 Headers =
                     {
-                        { "Authorization", "Bearer cSHsluz4Dz3jW1maa1FyhoFSFKmDrh2uZiEpFhyFIX2VUib6yQfyuLH5DpoVfvss" },
+                        { "cookie", "desired_location=https%253A%252F%252Fapi.genius.com%252Foauth%252Fauthorize; flash=%257B%257D; _csrf_token=0tyaBKoN382UVdHYzu5Ht2GXqDV%252BUvMP048zUmjMmlk%253D; _rapgenius_session=BAh7BzoPc2Vzc2lvbl9pZEkiJTFmNjNhNTYwMGEyZDE3MmFhNmVlZjUzMDFjZjJiZDk3BjoGRUY6EF9jc3JmX3Rva2VuSSIxMHR5YUJLb04zODJVVmRIWXp1NUh0MkdYcURWK1V2TVAwNDh6VW1qTW1saz0GOwZG--7d1a4faf0e4e4fc7c0b5ef2a5ffa3086143cf9d4; _genius_ab_test_cohort=30; _genius_ab_test_song_recommendations_v2=mixpanel" },
+                        { "Authorization", "Bearer YATwllDTBMVuLsxnz4x7yHMJwLuMPIee2anG8GTmMvNILzdPVwyCcD8O25uvzN1T" },
                     },
             };
             using (var response = await client.SendAsync(request))
