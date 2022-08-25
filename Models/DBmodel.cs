cae2fa7bfdba860441947470d9923750ba012bc3
idvele <ilari.katajamaki1@gmail.com>
2022-08-25 10:30:30 +0300

    Added DBmodel

---
+++ Models/DBmodel.cs
@@ -0,0 +1,29 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+
+namespace musicsearch.Models
+{
+    public class DBmodel
+    {
+        //[JsonProperty(PropertyName = "id")]
+        //public string Id { get; set; }
+
+        [JsonProperty(PropertyName = "Artist")]
+        public string Artist { get; set; }
+
+        [JsonProperty(PropertyName = "Song")]
+        public string Song { get; set; }
+
+        [JsonProperty(PropertyName = "Latitude")]
+        public string? Latitude { get; set; } = "0";
+
+        [JsonProperty(PropertyName = "Longitude")]
+        public string? Longitude { get; set; } = "0";
+
+
+    }
+}
